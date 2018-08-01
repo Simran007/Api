e0f4f47bd58e23ff2838002133ac8f1b285ed528
Simran Farooqui <farooquisimran6@gmail.com>
2018-08-01 16:46:20 +0530

    initial commit

---
+++ src/components/Api.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<button v-on:click="posts">
+			fetch posts
+		</button>
+
+		v-for="post in posts"
+
+		{{post.title}}
+		{{p}}
+	</div>
+</template>
+
+
+<script>
+
+	import axios from 'axios'
+	export default {
+      name:'rats',
+      data() {
+      	return {
+      		posts: []
+      	}
+      }
+      methods:{
+       posts:function(){
+       	  /*fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => console.log(json))*/
+
+         axios.get('https://jsonplaceholder.typicode.com/posts')
+         .then(function(response){
+             console.log(response.data)
+             this.$data.posts = response.data
+         })
+         .catch(function(error){
+         			console.log(error)
+         })
+         
+
+       }
+      }
+  }
+	
+</script>
